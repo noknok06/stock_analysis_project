c4729286c872551dffc80c579c9a0251f290014d
noknok06 <nok.h0618@gmail.com>
2025-06-17 00:06:23 +0900

    fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -60,7 +60,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # これが重要！
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
